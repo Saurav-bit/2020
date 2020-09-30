7ceb23307e2f56b01faafe664944eca92f945b85
Saurav-bit <56034679+Saurav-bit@users.noreply.github.com>
2020-10-01 00:09:40 +0530

    Add files via upload

---
+++ NumToString.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<string>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a=5;
+    float b=6.2;
+    string stri=to_string(a);
+    cout<<stri<<endl;
+
+}
